b2b1a8e86e68ae7cdc6b3400389153a2d717997e
Arjav Jain <arjavj25@iitk.ac.in>
2026-05-30 12:41:23 +0530

    Time: 31 ms (7.04%) | Memory: 68 MB (10.25%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -14,43 +14,42 @@ int idx;
         bucket[i].idx=i;
        }
        std::sort(bucket.begin(), bucket.end(), [](const area& a, const area& b) {
-        return a.key < b.key; 
-    });
-// int max_area=0;
-//     for(i=0;i<n;i++){
-//         int current1=0;
-//         int current2=0;
-//         for(int j=i+1;j<(n-j)/2 + 1;j++){
-//             current1=bucket[i].key*abs(bucket[i].idx-bucket[j].idx);
-//             current2=bucket[i].key*abs(bucket[i].idx-bucket[n-j].idx);
-//             max_area=max(max_area,max(current1,current2));
-           
-//         }
-//     }
-//   return max_area;
-vector<int> min_idx(n);
-        vector<int> max_idx(n);
-min_idx[n-1] = bucket[n-1].idx;
-        max_idx[n-1] = bucket[n-1].idx;
-        
-        for(int i = n - 2; i >= 0; i--) {
-            min_idx[i] = min(bucket[i].idx, min_idx[i+1]);
-            max_idx[i] = max(bucket[i].idx, max_idx[i+1]);
-        }
+        return a.key < b.key;   });
+ /*int max_area=0;
+     for(i=0;i<n;i++){
+         int current1=0;
+         int current2=0;
+         for(int j=i+1;j<(n-j)/2 + 1;j++){
+             current1=bucket[i].key*abs(bucket[i].idx-bucket[j].idx);
+             current2=bucket[i].key*abs(bucket[i].idx-bucket[n-j].idx);
+             max_area=max(max_area,max(current1,current2));         
+         }
+     }
+   return max_area;
+}
+};*/
 
         int max_area = 0;
 
-        // 3. Instead of a nested loop, evaluate the best pairing instantly
-        for(int i = 0; i < n - 1; i++){
-            // The best pair is either the element furthest to the left or furthest to the right
-            int dist1 = abs(bucket[i].idx - min_idx[i+1]);
-            int dist2 = abs(bucket[i].idx - max_idx[i+1]);
+        int global_min_idx = bucket[n - 1].idx;
+        int global_max_idx = bucket[n - 1].idx;
+
+        // 3. Loop backwards from the second-to-last element down to 0
+        for (int i = n - 2; i >= 0; i--) {
+            // Calculate distance to the furthest elements that are taller than bucket[i]
+            int dist1 = abs(bucket[i].idx - global_min_idx);
+            int dist2 = abs(bucket[i].idx - global_max_idx);
             
             int current = bucket[i].key * max(dist1, dist2);
             max_area = max(max_area, current);
+            
+            // Update the global min and max original indices to include the current one
+            global_min_idx = min(global_min_idx, bucket[i].idx);
+            global_max_idx = max(global_max_idx, bucket[i].idx);
         }
 
-        return max_area;
+        
+ return max_area;
     }
 
 
