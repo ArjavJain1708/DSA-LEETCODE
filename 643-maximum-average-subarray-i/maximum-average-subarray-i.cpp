eb1f32fb3b5d5e63b82b7b3140219e56c75a8633
Arjav Jain <arjavj25@iitk.ac.in>
2026-06-08 00:29:17 +0530

    Time: 0 ms (100.00%) | Memory: 113.7 MB (85.23%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-     double currentSum = 0;
+     double currentsum = 0;
         
-        // 1. Calculate the sum of the first 'k' elements
+        
         for (int i = 0; i < k; i++) {
-            currentSum += nums[i];
+            currentsum += nums[i];
         }
         
-        double maxAverage = currentSum / k;
+        double max_average = currentsum / k;
         
-        // 2. Slide the window across the rest of the array
+       
         for (int i = k; i < nums.size(); i++) {
-            // Add the new element, subtract the old one
-            currentSum += nums[i] - nums[i - k];
             
-            // Update maxAverage if the new one is higher
-            maxAverage = max(maxAverage, currentSum / k);
+            currentsum += nums[i] - nums[i - k];
+            
+            
+            max_average = max(max_average, currentsum / k);
         }
         
-        return maxAverage;
+        return max_average;
     }
 };
