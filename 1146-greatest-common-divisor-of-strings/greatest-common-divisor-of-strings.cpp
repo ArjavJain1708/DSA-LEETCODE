b87e8c3d3e30f5a355726a72c771351b4a401fd0
Arjav Jain <arjavj25@iitk.ac.in>
2026-06-16 20:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (32.87%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
     string gcdOfStrings(string str1, string str2) {
         if (str1 + str2 != str2 + str1) {
             return "";
         }
         
         // Step 2: Find the GCD of the lengths of both strings
-        int gcd_length = std::gcd(str1.length(), str2.length());
+        int gcd_length = findGCD(str1.length(), str2.length());
         
         // Step 3: Return the prefix of that GCD length
         return str1.substr(0, gcd_length);
