6a17251b16a3d1f3bd72a5fa014119b8fb56eb46
Arjav Jain <arjavj25@iitk.ac.in>
2026-06-25 19:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.19%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        unsigned int i;
+        for(i=0;i*i<=num;i++){
+            if(i*i==num){
+                return true;
+            }
+            
+        }
+  return false;  }
+};
