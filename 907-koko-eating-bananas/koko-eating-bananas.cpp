f0223259868254f1cf2967f0790725bdc335aca7
Arjav Jain <arjavj25@iitk.ac.in>
2026-06-30 19:52:38 +0530

    Time: 7 ms (70.77%) | Memory: 23 MB (47.16%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+   
+    bool canEatInTime(vector<int>& piles, int h, int k) {
+        long long hoursNeeded = 0;
+        for (int pile : piles) {
+            
+            hoursNeeded += (pile + k - 1) / k; 
+        }
+        return hoursNeeded <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+       
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = high; 
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (canEatInTime(piles, h, mid)) {
+                ans = mid;     
+                high = mid - 1; 
+            } else {
+                low = mid + 1; 
+            }
+        }
+        
+        return ans;
+    }
+
+};
