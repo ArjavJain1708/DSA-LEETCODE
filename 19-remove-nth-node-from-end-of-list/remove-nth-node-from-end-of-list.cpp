62b03d7cc1413ce3b700d31cd346fba5ab3bfaaf
Arjav Jain <arjavj25@iitk.ac.in>
2026-08-05 02:27:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (27.07%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,35 +11,39 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-   ListNode* prev = nullptr;
+    ListNode* prev = nullptr;
         ListNode* current = head;
         while (current != nullptr) {
-            ListNode* nextTemp = current->next; // Save next node
-            current->next = prev;               // Reverse the pointer
-            prev = current;                     // Advance prev
-            current = nextTemp;                 // Advance current
+            ListNode* nextTemp = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextTemp;
         }
         ListNode* reversedHead = prev;
 
-        // Step 2: Remove the n-th node from the reversed list (which is the n-th from the start now)
-        ListNode* dummy = new ListNode(0);
-        dummy->next = reversedHead;
-        
-        ListNode* curr = dummy;
-        int i = 1;
-        while (i < n && curr != nullptr && curr->next != nullptr) {
-            curr = curr->next;
-            i++;
-        }
-        
-        if (curr != nullptr && curr->next != nullptr) {
-            ListNode* nodeToDelete = curr->next;
-            curr->next = curr->next->next;
+        // Step 2: Remove the n-th node from the reversed list
+        // If n == 1, we are removing the first node of the reversed list
+        if (n == 1) {
+            ListNode* nodeToDelete = reversedHead;
+            reversedHead = reversedHead->next;
             delete nodeToDelete;
+        } 
+        else {
+            // Otherwise, traverse to the node just *before* the one we want to delete
+            ListNode* curr = reversedHead;
+            int i = 1;
+
+        while (i < n - 1 && curr != nullptr && curr->next != nullptr){
+                curr = curr->next;
+                i++;
         }
-        
-        reversedHead = dummy->next;
-        delete dummy;
+            
+            if (curr != nullptr && curr->next != nullptr) {
+                ListNode* nodeToDelete = curr->next;
+                curr->next = curr->next->next;
+                delete nodeToDelete;
+            }
+        }
 
         // Step 3: Reverse the list back to its original direction
         prev = nullptr;
