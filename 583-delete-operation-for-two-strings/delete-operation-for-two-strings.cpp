d8584b40c3a798a1b41c73aa9846b861002733ec
Arjav Jain <arjavj25@iitk.ac.in>
2026-08-13 19:46:03 +0530

    Time: 9 ms (72.46%) | Memory: 16.8 MB (55.52%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        
+        // Find LCS using standard 2D DP table
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        int lcs = dp[n1][n2];
+        
+        // Apply your exact formula
+        return n1 + n2 - 2 * lcs;
+    }
+};
